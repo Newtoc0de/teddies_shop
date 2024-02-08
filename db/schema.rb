334d3c0027a335a8c71fa5ebc51cdca78975929d
Clothilde Drouet clothildedrouet@gmail.com <clothildedrouet@gmail.com>
2024-02-08 11:56:00 +0100

    ajout prix à teddies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_08_103301) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_105504) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_08_103301) do
     t.string "photo_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price_cents", default: 0, null: false
     t.index ["category_id"], name: "index_teddies_on_category_id"
   end
 
